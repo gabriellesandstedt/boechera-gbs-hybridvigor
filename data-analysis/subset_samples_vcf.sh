31ddc1bb6d74e1c63cdd3a4a618cb5f9793fb52a
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2023-09-14 12:12:17 -0600

    Update subset_samples_vcf.sh

--- data-analysis/subset_samples_vcf.sh
+++ data-analysis/subset_samples_vcf.sh
@@ -32,9 +32,24 @@ rule noSRF2s:
         bcftools view -s "^$samples_to_remove" -o {output.noSRF2s_vcf} {input.vcf}
         """
 
+# define rule to subset vcf to remove retros hybrids 
+# bcftools: 
+rule noRhybrids:
+    input:
+        vcf=f"{data_dir}/{vcf}"
+    output:
+        noRhybrids_vcf=f"{data_dir}/allsamples_allsites_final_snps_subset_noRhybrids.vcf"
+    shell:
+        """
+        module load bcftools/1.16
+        echo -e '\\n['$(date)']\\n subset vcf ..\\n'
+        samples_to_remove="BIGC_RR,CAPG_RR,CG05_RR,DUNG_RR,LOBG_RR,LPAT_RR,MEYC_RR,MILL_RR,PLCU_RR,QCDH_RR,SQWC_RR,SVBR_RR,TWTQ_RR,WILC_RR,am1_1,am1_2,am1_3,am10_1,am10_2,am10_3,am11_1,am11_2,am11_3,am12_1,am12_2,am12_3,am13_1,am13_2,am13_3,am14_1,am14_2,am14_3,am15_1,am15_2,am15_3,am2_1,am2_2,am2_3,am3_1,am3_2,am3_3,am4_1,am4_2,am4_3,am5_1,am5_2,am5_3,am6_1,am6_2,am6_3,am7_1,am7_2,am7_3,am8_1,am8_2,am8_3,am9_1,am9_2,am9_3,wi1_1,wi1_2,wi1_3,wi10_1,wi10_2,wi10_3,wi2_1,wi2_2,wi2_3,wi3_1,wi3_2,wi3_3,wi4_1,wi4_2,wi4_3,wi5_1,wi5_2,wi5_3,wi6_1,wi6_2,wi6_3,wi7_1,wi7_2,wi7_3,wi8_1,wi8_2,wi8_3,wi9_1,am14_2b,wi2_1b,wi9_2,wi9_3"
+        bcftools view -s "^$samples_to_remove" -o {output.noRhybrids_vcf} {input.vcf}
+        """
+
 # define rule to subset vcf to 
 # bcftools: 
-rule noSRF2s:
+rule onlyRetros:
     input:
         vcf=f"{data_dir}/{vcf}"
     output:
