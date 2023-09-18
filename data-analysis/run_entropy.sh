8b3979565696a7c2a3006a55ba882becf0290c39
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2023-09-18 17:00:45 -0600

    Update run_entropy.sh

--- data-analysis/run_entropy.sh
+++ data-analysis/run_entropy.sh
@@ -16,3 +16,32 @@ entropy -i boech_gbs_retro_str_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_strict
 entropy -i boech_gbs_retro_retro_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_retro_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_retro_m1n2k2Q1l15000b5000t5s30_qfilek2_chain1.hdf5
 entropy -i boech_gbs_retro_retro_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_retro_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_retro_m1n2k2Q1l15000b5000t5s30_qfilek2_chain2.hdf5
 entropy -i boech_gbs_retro_retro_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_retro_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_retro_m1n2k2Q1l15000b5000t5s30_qfilek2_chain3.hdf5
+
+
+estpost.entropy -p q -s 4mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain1.hdf5 mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain2.hdf5 mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain1.hdf5 -o ent_stricta_retro_convergence.txt
+
+
+rule all:
+    input:
+        expand("mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain{chain}_att2.hdf5", chain=range(1, 4))
+
+rule entropy_retro_stricta:
+    input:
+        mgpl="boech_gbs_retro_stricta_gpl_entropy_final.mgpl",
+        qfile="retro_stricta_k2.txt"
+    output:
+        "mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain{chain}_att2.hdf5"
+    params:
+        m=1,
+        n=2,
+        k=2,
+        Q=1,
+        l=15000,
+        b=5000,
+        t=5,
+        s=30
+    shell:
+        """
+        entropy -i {input.mgpl} -m {params.m} -n {params.n} -k {params.k} -q {input.qfile} -Q {params.Q} -l {params.l} -b {params.b} -t {params.t} -s {params.s} -o {output}
+
+
