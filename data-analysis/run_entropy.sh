14f896c8ef64a93182096d00e866a03fa33b292d
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2023-09-18 16:24:16 -0600

    Create run_entropy.sh

---
+++ data-analysis/run_entropy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks=4 #parallel tasks/processes run on separate CPU cores or nodes (coarse-grained internode/intercore parallelization via MPI; requires OpenMPI/impi) Note: sbatch does not launch the tasks, only requests resources and submits the batch script. ntasks tells Slurm that N parallel tasks will be launched and to allocate resources accordingly. Parallel tasks are launched by the script.
+#SBATCH --mem=24G
+#SBATCH --job-name entropy
+#SBATCH --account=rushworth
+#SBATCH --partition=kingspeak-shared
+#SBATCH --output=ent.out
+#SBATCH --error=ent.error
+
+entropy -i boech_gbs_retro_str_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_stricta_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain1.hdf5
+entropy -i boech_gbs_retro_str_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_stricta_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain2.hdf5
+entropy -i boech_gbs_retro_str_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_stricta_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_stricta_m1n2k2Q1l15000b5000t5s30_qfilek2_chain3.hdf5
+
+entropy -i boech_gbs_retro_retro_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_retro_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_retro_m1n2k2Q1l15000b5000t5s30_qfilek2_chain1.hdf5
+entropy -i boech_gbs_retro_retro_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_retro_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_retro_m1n2k2Q1l15000b5000t5s30_qfilek2_chain2.hdf5
+entropy -i boech_gbs_retro_retro_entropy_final.mgpl -m 1 -n 2 -k 2 -q retro_retro_k2.txt -Q 1 -l 15000 -b 5000 -t 5 -s 30 -o mcmc_retro_retro_m1n2k2Q1l15000b5000t5s30_qfilek2_chain3.hdf5
